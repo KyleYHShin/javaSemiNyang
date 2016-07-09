3f864d878a8f22b11d22387315aa3873b0614649
YeonHo <shinpeach@naver.com>
2016-07-09 15:42:04 +0900

    게임화면 갱신 적용 완료 - 추가구현 필요부분 주석처리 해놓음

--- semiProject/src/controller/GameColorPanel.java
+++ semiProject/src/controller/GameColorPanel.java
@@ -11,76 +11,71 @@ import view.*;
 
 public class GameColorPanel extends JPanel {
 	private JPanel colorPanel;
+	private MainFrame mainFrame;
 	// 시작 레벨
-	private int Level = 2;
+	private int level;
 
-	public JPanel makeColorPanel(int Num) {
-		//게임용 패널 재생성
+	public JPanel makeColorPanel(int newLevel, MainFrame getFrame) {
+		this.level = newLevel;
+		// 기존에 있는 메인프레임 레퍼런스 저장
+		this.mainFrame = getFrame;
+		// 게임용 패널 재생성
 		colorPanel = new JPanel();
 		colorPanel.setSize(600, 600);
 		colorPanel.setLocation(0, 0);
-		colorPanel.setLayout(new GridLayout(Num, Num));
-		
-		//버튼 갯수 초기화
-		JButton[] Btn = new JButton[Num * Num];
-		//정답버튼 결정
+		colorPanel.setLayout(new GridLayout(level, level));
+		//////////////////////////////////////////////////////
+		// 일정레벨 이상일 때 칸 갯수 더이상 증가 못하게
+		// 색상 변경 알고리즘 추가->	Btn[i].setBackground(Color.BLACK);
+		//
+		//////////////////////////////////////////////////////
+		// 버튼 갯수 초기화
+		JButton[] Btn = new JButton[level * level];
+		// 정답버튼 결정
 		Random r = new Random();
-		int answer = (int) (r.nextInt(Num * Num));
-		addActionListener 
-		
+		int answer = (int) (r.nextInt(level * level));
+		// 이벤트 연결
+		GameButtonActionListener btnListener = new GameButtonActionListener();
+
 		// 조건에 맞게 버튼 생성
-		for (int i = 0; i < Num * Num; i++) {
+		for (int i = 0; i < level * level; i++) {
 			Btn[i] = new JButton();
-			Btn[i].setBackground(Color.YELLOW);
-			
-			if (i == answer){
+
+			if (i == answer) {
+				//버튼에 있는 text 안보이게 설정 필요
 				Btn[i].setText("정답");
-				Btn[i].addActionListener(l);
+				Btn[i].setBackground(Color.BLACK);
+				Btn[i].addActionListener(btnListener);
+			} else {
+				Btn[i].setText("오답");
+				Btn[i].setBackground(Color.YELLOW);
+				Btn[i].addActionListener(btnListener);
 			}
-				Btn[i].addActionListener(new ActionListener() {
-
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						JOptionPane.showMessageDialog(colorPanel, "정답입니다 !");
-						// 1.오류남
-						Level++;
-						MainFrame mf = new MainFrame();
-						mf.removeLevel();
-						mf.addLevel(Level);
-						// 2.오류는 안나지만 안뜸
-						// mf.newLevel();
-
-					}
-				});
-			else
-				Btn[i].addActionListener(new ActionListener() {
-
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						JOptionPane.showMessageDialog(colorPanel, "틀렸습니다.");
-
-					}
-				});
 			colorPanel.add(Btn[i]);
 		}
-		Btn[answer].setBackground(Color.BLACK);
 		return colorPanel;
 	}
 
-	// 내부클래스로 JRadioButton 이벤트 처리
+	// 내부클래스로 Button 이벤트 처리
 	private class GameButtonActionListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			JButton button = (JButton) e.getSource();
 
-			JOptionPane.showMessageDialog(colorPanel, "정답입니다 !");
-			// 1.오류남
-			Level++;
-			MainFrame mf = new MainFrame();
-			mf.removeLevel();
-			mf.addLevel(Level);
-			// 2.오류는 안나지만 안뜸
-			// mf.newLevel();
+			if (button.getText().equals("정답")) {
+				level++;
+				// 기존에 있는 메인프레임의 화면갱신 메서드(resetGamePanel()) 호출
+				mainFrame.resetGamePanel(level);
+			} else {
+				// 초기화면으로 돌아가기
+				mainFrame.initializeGamePanel();
+				// 종료화면 구성 추가
+				// "당신의 점수는 " + score + "점수를 업로드 하시겠습니까?" -> 예/아니오
+				// 예 -> 닉네임 기입 후 선택 -> 예/아니오
+				// 예 -> 연호 : 클라이언트.java 호출
+				JOptionPane.showMessageDialog(colorPanel, "게임종료\n점수를 업로드 하시겠습니까?");
+			}
+
 		}
 
 	}
--- semiProject/src/view/MainFrame.java
+++ semiProject/src/view/MainFrame.java
@@ -7,20 +7,23 @@ import javax.swing.event.*;
 import controller.*;
 
 public class MainFrame extends JFrame {
-	
+
 	private JPanel gamePanel;
 	private JPanel rankPanel;
 	private JPanel buttonPanel;
 	private JPanel colorPanel;
 	private JPanel scorePanel;
 	private JPanel progressbarPanel;
-	
-	//임의의 시작버튼 추가
+
+	// 임시 정의
+	private static MainFrame mainFrame;
+
+	// 임의의 시작버튼 추가
 	private JButton StartBtn;
-	
+
 	public static void main(String[] args) {
-		MainFrame mf = new MainFrame();
-		mf.setMainFrame();
+		mainFrame = new MainFrame();
+		mainFrame.setMainFrame();
 	}
 
 	public MainFrame() {
@@ -37,16 +40,13 @@ public class MainFrame extends JFrame {
 		gamePanel = new JPanel();
 		rankPanel = new JPanel();
 		buttonPanel = new JPanel();
-		colorPanel = new JPanel();
-		scorePanel = new JPanel();
-		progressbarPanel = new JPanel();
-		
+
 		gamePanel.setLayout(null);
-	    gamePanel.setSize(700, 850);
+		gamePanel.setSize(700, 850);
 		gamePanel.setLocation(0, 0);
 		this.add(gamePanel);
 
-	    rankPanel.setSize(300, 500);
+		rankPanel.setSize(300, 500);
 		rankPanel.setLocation(700, 0);
 		rankPanel.setBackground(Color.GREEN);
 		this.add(rankPanel);
@@ -54,71 +54,87 @@ public class MainFrame extends JFrame {
 		buttonPanel.setSize(300, 200);
 		buttonPanel.setLocation(700, 500);
 		buttonPanel.setBackground(Color.BLUE);
-		
-		//임의의 시작버튼 추가
-		StartBtn = new JButton("시작");
-		buttonPanel.add(StartBtn);
-		StartBtn.addActionListener(new ActionListener(){
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				GameColorPanel cp = new GameColorPanel();
-				colorPanel.add(cp.makeColorPanel(2));
-				colorPanel.updateUI();
-			}});
-		
+		// 게임 패널용 임시 시작버튼 추가////////////////////////
+		StartBtn = new JButton("시작");
+		startButtonActionListener startAction = new startButtonActionListener();
+		StartBtn.addActionListener(startAction);
+		buttonPanel.add(StartBtn);
 		this.add(buttonPanel);
-		
-		//gamePanel에 추가
+
+		// gamePanel 소속 패널들 초기화
+		scorePanel = new JPanel();
+		scorePanel.setLayout(null);
+		scorePanel.setSize(700, 100);
+		scorePanel.setLocation(0, 0);
+		scorePanel.setBackground(Color.ORANGE);
+
+		colorPanel = new JPanel();
+		colorPanel.setLayout(null);
+		colorPanel.setSize(600, 600);
+		colorPanel.setLocation(50, 100);
+		colorPanel.setBackground(Color.BLACK);
+
+		progressbarPanel = new JPanel();
+		progressbarPanel.setLayout(null);
+		progressbarPanel.setSize(700, 150);
+		progressbarPanel.setLocation(0, 700);
+		progressbarPanel.setBackground(Color.RED);
+
+		// gamePanel에 추가
 		gamePanel.add(scorePanel);
 		gamePanel.add(colorPanel);
 		gamePanel.add(progressbarPanel);
-		
-		addColorPanel();
-		addScorePanel();
-		addProgressbarPanel();
-		
+
 		this.setVisible(true);
 	}
 
-
-	public void addColorPanel(){
+	public void addColorPanel() {
 		colorPanel.setLayout(null);
 		colorPanel.setSize(600, 600);
 		colorPanel.setLocation(50, 100);
 		colorPanel.setBackground(Color.BLACK);
 	}
-	public void addScorePanel(){
+
+	public void addScorePanel() {
 		scorePanel.setLayout(null);
 		scorePanel.setSize(700, 100);
 		scorePanel.setLocation(0, 0);
 		scorePanel.setBackground(Color.ORANGE);
 	}
-	public void addProgressbarPanel(){
+
+	public void addProgressbarPanel() {
 		progressbarPanel.setLayout(null);
 		progressbarPanel.setSize(700, 150);
 		progressbarPanel.setLocation(0, 700);
 		progressbarPanel.setBackground(Color.RED);
 	}
-	
-	//1.레벨에 맞는 화면 추가
-	public void addLevel(int Level){
-		GameColorPanel cp = new GameColorPanel();
-		colorPanel.add(cp.makeColorPanel(Level));
-		
-	}
-	//기존 화면 삭제
-	public void removeLevel(){
-		
+
+	public void resetGamePanel(int level) {
+		// 패널에 있는 객체들 모두 삭제
 		colorPanel.removeAll();
-		colorPanel.updateUI();
+		// 새 객체(패널) 생성하여 추가
+		colorPanel.add(new GameColorPanel().makeColorPanel(level, this));
+		colorPanel.revalidate();
+		colorPanel.repaint();
 	}
-	
-	//2.새로 만들어서 추가 : 오류는 안났지만 안보임
-	public void newLevel(){
-		GameColorPanel cp = new GameColorPanel();
-		colorPanel = new JPanel();
-		colorPanel.add(cp.makeColorPanel(3));
-		colorPanel.updateUI();
+
+	//
+	public void initializeGamePanel() {
+		// 메인화면으로 돌아가기
+		// 게임 중이지 않을때의 초기화면 필요
+		// (게임명이라던가 단순한 이미지 뿌리기라던가...)
+		colorPanel.removeAll();
+		colorPanel.setBackground(Color.BLACK);
+		colorPanel.revalidate();
+		colorPanel.repaint();
+	}
+
+	// 내부클래스로 startButton 이벤트 처리
+	private class startButtonActionListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			mainFrame.resetGamePanel(2);
+		}
 	}
 }
