f5ce8c6addcb2a27f7ed55e09478bb16b72952b0
silencejini <silencejini@naver.com>
2016-07-12 18:04:33 +0900

    색상 결정 및 색상 난이도 조절 추가

--- semiProject/src/view/MainFrame.java
+++ semiProject/src/view/MainFrame.java
@@ -1,69 +1,87 @@
 package view;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.event.*;
-import java.io.File;
-import javax.imageio.ImageIO;
-import controller.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Rectangle;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import view.game.GameView;
+import view.rank.RankView;
+import view.button.ButtonView;
+
+import controller.game.GameController;
+import model.Linker;
 
 public class MainFrame extends JFrame {
-
-	// 임시 정의
-	private static MainFrame mainFrame;
-	
+	//각 객체 노드 저장
+	private Linker link;
 	
 	private JPanel gamePanel;
 	private JPanel rankPanel;
 	private JPanel buttonPanel;
+
+	private GameView gameView;
+	private RankView rankView;
+	private ButtonView buttonView;
 	
-	
-	public static void main(String[] args) {
-		mainFrame = new MainFrame();
-		mainFrame.setMainFrame();
-	}
+	private GameController gameController;
 
 	public MainFrame() {
+		link = new Linker();
+		link.setMainFrame(this);
+		
+		gameController = new GameController(link);
+		gameView = new GameView(link);
+		buttonView = new ButtonView(link);
+		rankView = new RankView(link);
 	}
 
 	public void setMainFrame() {
-		
-		ButtonView buttonView = new ButtonView();
-		GameView gameView = new GameView();
-		
-		// 윈도우 창 초기화 --------------------------------
+		// 윈도우 창 초기화 ----------------------------------------
 		this.setTitle("반응하라! 절대 색감");
 		this.setBounds(new Rectangle(0, 0, 1000, 850));
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-
-		// 크기 변경 불가
 		this.setLayout(null);
+		
 		gamePanel = new JPanel();
 		rankPanel = new JPanel();
 		buttonPanel = new JPanel();
 
-		this.setLayout(null);
-		gamePanel.setSize(700, 850);
-		gamePanel.setLocation(0, 0);
-		gamePanel.add(gameView.makeGameView());
+		gamePanel.setBounds(0, 0, 700, 850);
+		gamePanel.setLayout(null);
+		gamePanel.setBackground(Color.RED);// 테스트
+		gamePanel.add(link.getGameView().getGameTotalPanel());
 		this.add(gamePanel);
 
-		this.setLayout(null);
-		rankPanel.setSize(300, 650);
-		rankPanel.setLocation(700, 0);
-		rankPanel.setBackground(Color.GREEN);
-		this.add(rankPanel);
-
-		this.setLayout(null);
-		buttonPanel.setSize(300, 200);
-		buttonPanel.setLocation(700, 650);
+		buttonPanel.setBounds(700, 650, 300, 200);
 		buttonPanel.setBackground(Color.BLUE);
 		buttonPanel.add(buttonView.makeButtonView());
 		this.add(buttonPanel);
-		
-		
+
+		rankPanel.setLayout(new BorderLayout());
+		rankPanel.setBounds(700, 0, 300, 650);
+		rankPanel.setBackground(Color.GREEN);
+		rankPanel.add(rankView.makeRankView());
+		this.add(rankPanel);
+
 		this.setVisible(true);
 	}
 	
+	public GameController getGameController(){
+		return gameController;
+	}
+
+	public GameView getGameView() {
+		return gameView;
+	}
+
+	public RankView getRankView() {
+		return rankView;
+	}
+
+	public ButtonView getButtonView() {
+		return buttonView;
+	}
 }
